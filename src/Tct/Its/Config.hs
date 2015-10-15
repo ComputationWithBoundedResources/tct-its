6af5d33c5ae414939183c3d8f229f6e54c352af4
mschaper <michael.schaper@uibk.ac.at>
2015-10-15 18:14:30 +0200

    set default strategy

--- src/Tct/Its/Config.hs
+++ src/Tct/Its/Config.hs
@@ -24,6 +24,7 @@ type ItsConfig = T.TctConfig Its
 
 itsConfig :: ItsConfig
 itsConfig = T.defaultTctConfig fromFile
+  `withDefaultStrategy` runtime
   `addStrategies`
     [ T.SD runtimeDeclaration ]
 
