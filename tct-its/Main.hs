23ea9d396ac44056147bd90709270bab556ca59c
mschaper <michael.schaper@uibk.ac.at>
2018-02-15 11:40:48 +0100

    remove looptree processor (2)

--- tct-its/Main.hs
+++ tct-its/Main.hs
@@ -2,9 +2,8 @@ module Main (main) where
 
 import Tct.Core
 import Tct.Its
-import Tct.Its.Processor.Looptree (cyclemania, looptree)
 
-instance Declared Its Its where decls = SD looptree : SD cyclemania : itsDeclarations
+instance Declared Its Its where decls = itsDeclarations
 
 main :: IO ()
 main = runIts itsConfig
